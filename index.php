7e4bd35275b8c1669c5750c1bf32fbb9b21c6086
avsholeh <avsholeh@gmail.com>
2020-05-11 22:40:32 +0700

    Menambahkan kerangka script

--- index.php
+++ index.php
@@ -3,10 +3,6 @@
 
 <head>
     <meta charset="UTF-8">
-
-    <!-- debugging purpose -->
-    <meta http-equiv="refresh" content="5">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <title>Simulasi Kredit</title>
@@ -21,7 +17,7 @@
 <body>
 
     <main>
-        <form>
+        <form id="simulasiKredit">
             <h1 class="h3 mb-3">Simulasi Kredit</h1>
 
             <div class="form-group">
@@ -41,18 +37,18 @@
 
             <div class="form-group">
                 <div class="form-check-inline">
-                    <input type="radio" class="form-check-input" name="metode" id="metode1">
-                    <label class="form-check-label" for="metode1">Flat</label>
+                    <input type="radio" class="form-check-input" value="Flat" name="metode" id="Flat" checked>
+                    <label class="form-check-label" for="Flat">Flat</label>
                 </div>
 
                 <div class="form-check-inline">
-                    <input type="radio" class="form-check-input" name="metode" id="metode2">
-                    <label class="form-check-label" for="metode2">Efektif</label>
+                    <input type="radio" class="form-check-input" value="Efektif" name="metode" id="Efektif">
+                    <label class="form-check-label" for="Efektif">Efektif</label>
                 </div>
 
                 <div class="form-check-inline">
-                    <input type="radio" class="form-check-input" name="metode" id="metode3">
-                    <label class="form-check-label" for="metode3">Anuitas</label>
+                    <input type="radio" class="form-check-input" value="Anuitas" name="metode" id="Anuitas">
+                    <label class="form-check-label" for="Anuitas">Anuitas</label>
                 </div>
             </div>
 
@@ -67,7 +63,31 @@
 <script src="assets/jquery/jquery-3.5.1.min.js"></script>
 <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 <script>
-    
+    $(document).ready(function() {
+        let jumlahKredit = $('#jumlahKredit');
+        let jangkaWaktu = $('#jangkaWaktu');
+        let bungaPertahun = $('#bungaPertahun');
+        let metode = $("#simulasiKredit input[name=metode]:checked");
+
+        $("#btnKalkulasi").click(function(e) {
+            e.preventDefault();
+            console.log(metode.val());
+        });
+
+        $("#btnUlangi").click(function(e) {
+            e.preventDefault();
+        });
+
+        function hitung(metode) {
+            if (metode == "Anuitas") {
+                // hitung anuitas
+            } else if (metode == "Efektif") {
+                // hitung efektif
+            } else {
+                // hitung flat
+            }
+        }
+    });
 </script>
 
 </html>
