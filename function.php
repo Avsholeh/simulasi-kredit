b57f46f38c0a6fe0e3e02854f5779d908210dfb6
avsholeh <avsholeh@gmail.com>
2020-05-12 09:30:45 +0700

    Menambahkan function metode_flat, perhitungan bunga dan pokok perbulan.

--- function.php
+++ function.php
@@ -1,13 +1,17 @@
 <?php
 header("Content-Type:application/json");
 
-if (isset($_GET['param'])) {
+define("HARI_BULAN", 30);
+define("HARI_TAHUN", 360);
+define("BULAN_TAHUN", 12);
+
+// if (isset($_POST['param'])) {
     
-} else {
+// } else {
 
-    response("param");
+//     response("param");
 
-}
+// }
 
 function response($param){
     $response['param'] = $param;
@@ -15,15 +19,41 @@ function response($param){
     echo $json_response;
 }
 
-function metode_flat() {
+function metode_flat($jumlahPinjaman, $jangkaWaktu, $sukuBunga) {
+    $data = [];
+    $pokok = pokokPerbulan($jumlahPinjaman, $jangkaWaktu);
+    $bunga = bungaPerbulan($jumlahPinjaman, $sukuBunga);
+    $sisaPinjaman = $jumlahPinjaman;
+    $jumlahAngsuran = ( $pokok + $bunga );
 
+    for($i = 0; $i < $jangkaWaktu; $i++) {
+        $sisaPinjaman -= $pokok;
+        array_push($data, [
+            "no"                => $i + 1,
+            "pokok"             => round($pokok),
+            "bunga"             => round($bunga),
+            "jumlahAngsuran"    => round($jumlahAngsuran),
+            "sisaPinjaman"      => round($sisaPinjaman)
+        ]);
+    }
+    return $data;
 }
 
 function metode_efektif() {
-
+    
 }
 
 function metode_anuitas() {
 
 }
+
+function bungaPerbulan($pokokPinjaman, $sukuBunga) {
+    return ($pokokPinjaman * $sukuBunga) / BULAN_TAHUN;
+}
+
+function pokokPerbulan($pokokPinjaman, $jangkaWaktu) {
+    return $pokokPinjaman / $jangkaWaktu;
+}
+
+echo var_dump(metode_flat(100000000, 12, 11/100));
 ?>
